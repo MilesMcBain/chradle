44495b4a800be2235795c0a48969a867d082c4bc
Miles McBain <miles.mcbain@gmail.com>
2018-09-05 15:56:12 +1000

    Added chradle skeleton.

---
+++ R/chradle.R
@@ -0,0 +1,81 @@
+Chradle <-
+  R6::R6Class("Chradle",
+              public = list(
+                ws_con = NULL
+                session_num = NULL,
+                debug_port = NULL,
+                session_url = NULL,
+                message_ctr = NULL,
+                response = NULL,
+                waiting_for_reply = NULL,
+                initialize = function(url = "http://localhost:8080", debug_port=9222, bin="chromium-browser"){
+
+                  self$session_num <- 1
+                  self$session_url <- url
+                  self$message_ctr <- 1
+                  self$waiting_for_reply <- FALSE
+
+                  ## launch chromium window
+                  processx::process$new("chromium-browser",
+                                        c("--new-window",
+                                          url,
+                                          "--user-data-dir=remote-profile",
+                                          glue::glue("--remote-debugging-port={debug_port}")))
+
+                  open_debugegrs <-
+                    jsonlite::read_json(glue::glue("http://localhost:{debug_port}/json"))
+                  ws_addr <- open_debugegrs[[self$session_num]]$webSocketDebuggerUrl
+                  self$ws_con <- websocket::WebSocket$new(ws_addr, autoConnect = FALSE)
+
+                  self$ws_con$onMessage(function(event){
+                    self$response <- event$data
+                    self$waiting_for_reply <- FALSE
+                  })
+
+                  self$ws_con$connect()
+
+                  self$send_message(method = "Runtime.enable")
+                },
+
+                get_message_id = function(){
+                  current_id <- self$message_ctr
+                  self$message_ctr <- self$message_ctr + 1
+                  current_id
+                },
+
+                send_message = function(method, params = NULL){
+                  args <- list()
+                  args$id <- self$get_message_id()
+                  args$method <- method
+                  args$params <- params
+                  package <- jsonlite::toJSON(args, auto_unbox = TRUE)
+                  self$ws_con$send(package)
+                }
+
+                get_attribute_names = function(id){
+                  self$waiting_for_reply <- TRUE
+                  self$send_message(method = "Runtime.evaluate",
+                                    params = list(
+                                      expression = glue::glue('document.querySelector("{id}").getAttributeNames()')))
+                  f <- future({self$get_reply()})
+                  value(f)
+                },
+
+                get_attributes = function(id, attribute){
+                  self$waiting_for_reply <- TRUE
+                  self$send_message(method = "Runtime.evaluate",
+                                    params = list(
+                                      expression = glue::glue('document.querySelector("{id}").getAttribute("{attribute}")')))
+                  f <- future({self$get_reply()})
+                  value(f)
+                }
+
+              ),
+
+              private = list(
+
+                get_reply = function(){
+                  while(self$waiting_for_reply){}
+                  self$response
+                })
+              )
