030c369f3bfaa20a806d848d9f642b4de40e311e
Miles McBain <miles.mcbain@gmail.com>
2018-09-05 16:01:14 +1000

    Allow custom binary. Add glue as import.

--- R/chradle.R
+++ R/chradle.R
@@ -16,7 +16,7 @@ Chradle <-
                   self$waiting_for_reply <- FALSE
 
                   ## launch chromium window
-                  processx::process$new("chromium-browser",
+                  processx::process$new(glue::glue("{bin}"),
                                         c("--new-window",
                                           url,
                                           "--user-data-dir=remote-profile",
